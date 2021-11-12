19ffa8d27f3b6df339285239adbd0f67ea9d1b72
jfeng3456j <jfeng3456j@gmail.com>
2021-11-12 11:04:08 -0600

    jsx and tenary operator concepts

--- src/App.js
+++ src/App.js
@@ -2,8 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  //class method
+  bar = () => 'foo';
   render() {
-      <h1>Master branch</h1>
+    const name = "john";
+
+    const foo = () => 'Bar';
+
+    const loading = true;
+
+    if (loading) {
+       return <h4> Loading...</h4>
+    }
+
+    return (
+      <div className='App'>
+        <h1>Hello {name.toUpperCase()} </h1>
+        <p>Expression: {1 + 2}</p>
+        <p>Function hello: {foo()}</p>
+        <p>Class Function hello: {this.bar()}</p>
+        <p>ternary operator:</p>
+        {loading ? <h4> Loading...</h4> : <h4> data ...</h4>}
+        </div>
+      )
   }
 }
 
